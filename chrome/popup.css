6a20490d2fdefcc1f8efe6dfc659ff8a314e9e5a
Guilherme Nascimento <brcontainer@yahoo.com.br>
2016-07-06 22:17:55 -0300

    0.0.8 version

--- chrome/popup.css
+++ chrome/popup.css
@@ -1,5 +1,5 @@
 /*
- * StackExchangeNotifications 0.0.7
+ * StackExchangeNotifications 0.0.8
  * Copyright (c) 2016 Guilherme Nascimento (brcontainer@yahoo.com.br)
  * Released under the MIT license
  *
